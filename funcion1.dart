3619a6c67e8102847ce4d241c0865fdb7e3c2d4d
Alex1ssss <alexissanchez99998@gmail.com>
2025-02-07 16:47:43 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+// funcion que no devuelve nada
+// funcion saludar
+void saludar(){
+  print("Hello World");
+}
+void escribeNombre(){
+  print("Alexis Espino Sanchez");
+}
+
+void main(){
+  //Alexis Espino Sanchez a.22308051281062
+  //llamar a la funcion saludar
+  print("Alexis Espino Sanchez a.22308051281062");
+  print("llamar a la funcion saludar");
+  saludar();
+  print("llamando a la funcion escribeNombre");
+  escribeNombre();
+}
