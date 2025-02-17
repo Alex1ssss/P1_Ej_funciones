40653b6d783a265ab16ff40619f3361cbcfe1025
Alex1ssss <alexissanchez99998@gmail.com>
2025-02-17 18:19:29 +0000

    ejemplo salida

---
+++ clase1.dart
@@ -0,0 +1,33 @@
+class Persona {
+  String nombre;
+  int edad;
+
+  // Constructor
+  Persona(this.nombre, this.edad);
+
+  // Método para saludar
+  void saludar() {
+    print("Hola soy $nombre");
+  }
+
+  // Método para cumplir años
+  void cumpliranos() {
+    edad++;
+    print("Feliz cumpleaños $nombre! Ahora tienes $edad años.");
+  }
+}
+
+void main() {
+  print("Alexis Espino Sanchez a.22308051281062");
+
+  // Crear una instancia de Persona
+  Persona persona = Persona("Alexis", 20);
+
+  // Acceder a los atributos
+  print("nombre: ${persona.nombre}");
+  print("edad: ${persona.edad}");
+
+  // Llamar a los métodos
+  persona.saludar();
+  persona.cumpliranos(); // Aquí llama al nuevo método para incrementar la edad
+}
